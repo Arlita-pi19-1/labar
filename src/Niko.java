67a1b8ebaa070e136a7f49a471fd44427a66b586
Arlita-pi19-1 <protopopova.aelita@gmail.com>
2020-09-11 20:34:27 +0900

    5,5

---
+++ src/Niko.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.lang.System;
+import java.util.Scanner;
+import java.io.File;
+import java.io.DataInputStream;
+
+public class Niko {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите имя файла => ");
+        String fname = sc.nextLine();
+        try {
+// Создается файл
+            File f1 = new File(fname);
+            f1.createNewFile(); // файл создан
+            System.out.println("Полный путь файла: " + f1.getAbsolutePath());
+
+            System.out.print("Введите количество строк для записи в файл => ");
+            int n = sc.nextInt();
+
+            DataOutputStream dOut =
+                    new DataOutputStream(new FileOutputStream(f1));
+            sc.nextLine(); //очистка буфера
+            for (int i = 0; i < n; i++) {
+                System.out.print("Введите строку для записи в файл => ");
+                String s = sc.nextLine();
+                dOut.writeUTF(s);
+
+            }
+            dOut.flush(); // дописываем несохраненные данные на диск
+            dOut.close(); // закрываем поток
+
+// Чтение и вывод данных из созданного файла
+            DataInputStream dIn = new DataInputStream(new FileInputStream(f1));
+            while (true) {
+                System.out.println(dIn.readUTF());
+            }
+        } catch (Exception e) {
+            System.out.println("" + e);
+
+        }
+    }
+}
